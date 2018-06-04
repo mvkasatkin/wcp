872adee4cf6fd5731a046f4cc7fccdf8dbb8c728
mvkasatkin <mvkasatkin@gmail.com>
2018-06-04 13:12:56 +0700

    dev

--- src/admin/shared/transitions/TransitionComeIn.tsx
+++ src/admin/shared/transitions/TransitionComeIn.tsx
@@ -1,32 +1,14 @@
 import * as React from 'react'
 import { Animate } from 'react-move'
-import * as Ease from 'd3-ease'
+import { TransitionParams } from '../../config/transitions'
 
 interface Props {
-  height: number,
-  duration: number,
+  params: TransitionParams,
   children: React.ReactElement<any>,
-  delay?: number,
-  ease?: (n: number) => number,
 }
 
 export default function (props: Props) {
-  const { children, duration, height } = props
-  const params = {
-    show: true,
-    start: { },
-    enter: {
-      opacity: [0, 1],
-      transform: (t: number) => `translateY(${height * (1 - t)}px) scale(${0.5 + t / 2})`,
-      timing: {
-        duration,
-        delay: props.delay || 0,
-        ease: props.ease || Ease.easeCubicOut,
-      },
-    },
-    update: {},
-    leave: {},
-  }
+  const { children, params } = props
 
   return (
     <Animate {...params}>
